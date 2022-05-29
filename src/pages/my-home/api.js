29e0b0bad38b5c7d1fa1cb6b5dec28c90f1a6625
yuanhao <huzhen@inke.cn>
2022-05-29 19:13:47 +0800

    deploy

--- src/pages/my-home/api.js
+++ src/pages/my-home/api.js
@@ -1,6 +1,6 @@
 import ApiUrl from "src/api/apiUrl";
 import api from "src/api";
-const { get } = api({ baseURL: ApiUrl.root });
+// const { get } = api({ baseURL: ApiUrl.root });
 export const getList = () => {
   return get("");
 };
