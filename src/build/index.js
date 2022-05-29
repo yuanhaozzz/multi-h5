a6275e4490370ebdd858e7b2f0d72855521df3fd
yuanhao <huzhen@inke.cn>
2022-05-29 15:47:01 +0800

    deploy

--- src/build/index.js
+++ src/build/index.js
@@ -4,7 +4,7 @@ module.exports = {
   "my-home": getPageBaseInfo({
     title: "首页",
   }),
-  // "my-test": getPageBaseInfo({
-  //   title: "测试页面",
-  // }),
+  "my-test": getPageBaseInfo({
+    title: "测试页面",
+  }),
 };
