c82bb4975a66120e38cc539d04b8f243ef8c73f2
yuanhao <huzhen@inke.cn>
2022-05-29 11:01:52 +0800

    deploy

--- src/pages/my-home/app.js
+++ src/pages/my-home/app.js
@@ -43,6 +43,7 @@ class Home extends Component {
   render() {
     return (
       <div className="home-wrapper">
+        222
         <div className="bomb-animation"></div>
         <h2>欢迎来到浩哥多页面移动端模版</h2>
         <audio src={Music} controls></audio>
