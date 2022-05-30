a323c16f6037c98c50444ba4eda0e42c23cf0198
yuanhao <huzhen@inke.cn>
2022-05-30 17:50:09 +0800

    deploy

--- src/build/index.js
+++ src/build/index.js
@@ -1,13 +1,13 @@
 const { getPageBaseInfo } = require("./utils");
 
 module.exports = {
-  "my-home": getPageBaseInfo({
-    title: "首页",
-  }),
+  // "my-home": getPageBaseInfo({
+  //   title: "首页",
+  // }),
   // "my-test": getPageBaseInfo({
   //   title: "测试页面",
   // }),
-  // "my-news": getPageBaseInfo({
-  //   title: "news",
-  // }),
+  "my-news": getPageBaseInfo({
+    title: "news",
+  }),
 };
