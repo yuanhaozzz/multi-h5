038ae5677ea917ab15ee46ed8eaee973253e919b
yuanhao <yuanhao_zzz@163.com>
2022-06-06 15:29:35 +0800

    deploy

--- config/git/pre-commit.js
+++ config/git/pre-commit.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const path = require("path");
+const pages = require("../../src/build");
+
 const { logError, execStr, grren } = require("../common");
 
 /**
@@ -91,11 +95,48 @@ function checkEslint() {
   }
 }
 
+/**
+ * 读取yml模板配置文件
+ * @param {*} filename
+ */
+const readFile = (filename) =>
+  fs
+    .readFileSync(path.join(__dirname, "..", "yml", filename), "utf8")
+    .toString();
+
+/**
+ * @description 重写部署文件
+ */
+function resetYmlFile() {
+  let file = readFile("deploy.yml");
+  const pageKeys = Object.keys(pages);
+  pageKeys.forEach((page) => {
+    file += `
+   - name: 打包页面：${page} 
+   uses: easingthemes/ssh-deploy@v2.0.7
+   env:
+     SSH_PRIVATE_KEY: \${{ secrets.DEPLOY_KEY }}
+     ARGS: "-avzr --delete"
+     SOURCE: "dist/${page}"
+     REMOTE_HOST: \${{ secrets.SSH_HOST }}
+     REMOTE_USER: \${{ secrets.SSH_USERNAME }}
+     TARGET: "/root/web/multi-h5/dist/"
+   `;
+  });
+  // 重写deploy配置文件
+  fs.writeFileSync(
+    path.join(__dirname, "../../.github/workflows", "deploy.yml"),
+    file
+  );
+}
+
 function check() {
   // 检查冲突
   // checkGitStageFileConflict();
   // eslint检查
   // checkEslint();
+  // 重写打包配置文件
+  resetYmlFile();
 }
 
 check();
