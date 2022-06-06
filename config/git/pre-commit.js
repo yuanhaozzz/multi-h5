dba0ec25e879a26ca6d2dac021e40a18579ac4a5
yuanhao <yuanhao_zzz@163.com>
2022-06-06 15:43:22 +0800

    deploy

--- config/git/pre-commit.js
+++ config/git/pre-commit.js
@@ -112,15 +112,15 @@ function resetYmlFile() {
   const pageKeys = Object.keys(pages);
   pageKeys.forEach((page) => {
     file += `
-   - name: deploy ${page} 
-   uses: easingthemes/ssh-deploy@v2.0.7
-   env:
-     SSH_PRIVATE_KEY: \${{ secrets.DEPLOY_KEY }}
-     ARGS: "-avzr --delete"
-     SOURCE: "dist/${page}"
-     REMOTE_HOST: \${{ secrets.SSH_HOST }}
-     REMOTE_USER: \${{ secrets.SSH_USERNAME }}
-     TARGET: "/root/web/multi-h5/dist/"
+      - name: deploy ${page} 
+      uses: easingthemes/ssh-deploy@v2.0.7
+      env:
+        SSH_PRIVATE_KEY: \${{ secrets.DEPLOY_KEY }}
+        ARGS: "-avzr --delete"
+        SOURCE: "dist/${page}"
+        REMOTE_HOST: \${{ secrets.SSH_HOST }}
+        REMOTE_USER: \${{ secrets.SSH_USERNAME }}
+        TARGET: "/root/web/multi-h5/dist/"
    `;
   });
   // 重写deploy配置文件
